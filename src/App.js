98298543d47ebbb57e1505108384dd4c8995003e
Neve Foresti <neve.foresti@gmail.com>
2020-03-27 19:29:41 -0700

    tried to fix bug. The only thing that doesn't work is the location filter. see line 135 app.js

--- src/App.js
+++ src/App.js
@@ -110,6 +110,8 @@ class App extends PureComponent {
 
   //filtering existing results
   filterResults(filters) {
+    console.log("this.state.records " + this.state.records);
+    console.log(filters);
     let results = this.state.records.filter(function(record) {
       if (!record.fields["Activity Name"]) {
         record.fields["Activity Name"] = "";
@@ -130,6 +132,7 @@ class App extends PureComponent {
         record.fields["Device Required"] = "";
       }
       return (
+        ///want to change location includes to ==, but causes filter error O.o I HAVE NO IDEA WHYYYY!!!!!
         record.fields["Location"].includes(filters.place) &&
         record.fields["Device Required"].includes(filters.screens) &&
         record.fields["Recommended Ages"].includes(filters.age) &&
@@ -138,8 +141,9 @@ class App extends PureComponent {
           record.fields["Activity Name"].includes(filters.search))
       );
     });
-
+    console.log("results " + results);
     this.setState({
+      viewableResults: results,
       filteredRecords: results,
       page:1
     });
